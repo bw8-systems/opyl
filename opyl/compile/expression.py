3bfc3a97ebffd23a87a16f1c1a549f6c4e5dee46
bwburnsides <19192452+bwburnsides@users.noreply.github.com>
2023-11-13 08:08:37 -0600

    pratt combinator

--- opyl/compile/expression.py
+++ opyl/compile/expression.py
@@ -1,52 +1,54 @@
 from opyl import nodes
 from opyl.compile.nodes import BinaryOperator, BinaryExpression
 
+from opyl import combinators as comb
 from opyl.compile.lexemes import IntegerLiteral, Primitive, PrimitiveKind
 from opyl.compile.positioning import Span, Stream
 
 type Token = IntegerLiteral | Primitive
 
 
-def parse(tokens: Stream[Token]) -> nodes.Expression | None:
-    return pratt(0, tokens)
+# TODO: Make infallible.
+class ExpressionParser(comb.Combinator[nodes.Expression | None]):
+    def __call__(self) -> nodes.Expression | None:
+        return self.pratt(0)
 
+    def prefix(self) -> nodes.Expression | None:
+        peeked = self.stream.peek()
+        if isinstance(peeked, IntegerLiteral):
+            self.stream.next()
+            return nodes.IntegerLiteral(peeked.span, peeked.integer)
 
-def prefix(tokens: Stream[Token]) -> nodes.Expression | None:
-    peeked = tokens.peek()
-    if isinstance(peeked, IntegerLiteral):
-        tokens.next()
-        return nodes.IntegerLiteral(peeked.span, peeked.integer)
-
-    return None
-
-
-def pratt(precedence_limit: int, tokens: Stream[Token]) -> nodes.Expression | None:
-    left = prefix(tokens)
-    if left is None:
         return None
 
-    return pratt_loop(precedence_limit, left, tokens)
+    def pratt(self, precedence_limit: int) -> nodes.Expression | None:
+        left = self.prefix()
+        if left is None:
+            return None
 
+        return self.pratt_loop(precedence_limit, left)
 
-def pratt_loop(
-    precedence_limit: int, left: nodes.Expression, tokens: Stream[Token]
-) -> nodes.Expression | None:
-    peeked = tokens.peek()
-    if not (isinstance(peeked, Primitive) and peeked.kind in BinaryOperator.values()):
-        return left
+    def pratt_loop(
+        self, precedence_limit: int, left: nodes.Expression
+    ) -> nodes.Expression | None:
+        peeked = self.stream.peek()
+        if not (
+            isinstance(peeked, Primitive) and peeked.kind in BinaryOperator.values()
+        ):
+            return left
 
-    operator = BinaryOperator(peeked.kind)
-    precedence = operator.precedence()
-    if precedence <= precedence_limit:
-        return left
+        operator = BinaryOperator(peeked.kind)
+        precedence = operator.precedence()
+        if precedence <= precedence_limit:
+            return left
 
-    tokens.next()
-    right = pratt(precedence, tokens)
-    if right is None:
-        return None
+        self.stream.next()
+        right = self.pratt(precedence)
+        if right is None:
+            return None
 
-    new_left = BinaryExpression(Span.default(), operator, left, right)
-    return pratt_loop(precedence_limit, new_left, tokens)
+        new_left = BinaryExpression(Span.default(), operator, left, right)
+        return self.pratt_loop(precedence_limit, new_left)
 
 
 if __name__ == "__main__":
@@ -98,5 +100,6 @@ if __name__ == "__main__":
             span=Span.default(),
         ),
     ]
-    parsed = parse(Stream(tokens))
-    pprint(parsed)
+
+    parser = ExpressionParser(Stream(tokens))
+    pprint(parser())
