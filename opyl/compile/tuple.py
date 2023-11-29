80dcc909f4307bb40fab71e8503fa1d873ca005f
bwburnsides <19192452+bwburnsides@users.noreply.github.com>
2023-11-28 21:40:07 -0600

    remove test files

--- opyl/compile/tuple.py
+++
@@ -1,9 +0,0 @@
-import typing as t
-
-
-def wrapper(members: tuple[tuple[str, type], ...]):
-    return t.NamedTuple("SomeType", members)
-
-
-Point = wrapper((("foo", int), ("bar", int)))
-p = Point(1, 2)
